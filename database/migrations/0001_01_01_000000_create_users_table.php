90de128099a0f2cca93dddda90afbbe75602676b
loise meline <loise13@hotmail.fr>
2024-06-27 11:54:06 +0200

    creation des tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('prenom');
+            $table->date('date_naissance');
+            $table->date('adresse');
+            $table->date('code_postal');
+            $table->date('telephone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
